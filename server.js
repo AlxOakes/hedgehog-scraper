af520ecc200e357e5fd1a5d2958ca786258de899
AlxOakes <45518888+AlxOakes@users.noreply.github.com>
2018-12-02 11:16:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,45 @@ app.get('/', (req, res) => {
   res.send("Hedgehog Time");
 });
 
+
+app.get('/hedgie', (req, res) => {
+  res.send("Hedgehog TIME");
+});
 // your endpoint
+app.get('/hedgie/:keyword', (req, res) => {
+  var keyword = req.params.keyword;
+
+  function findHedgieImage(keyword) {
+    var nightmare = Nightmare({ show: true });
+
+    return nightmare
+    .goto('https://www.google.com')
+    .insert('input[title="Search"]', `hedgehog ${keyword}`)
+    .click('input[value="Google Search"]')
+    .wait('a.q.qs')
+    .click('a.q.qs')
+    .wait('div#res.med')
+    .evaluate(function() {
+        var photoDivs = document.querySelectorAll('img.rg_ic');
+        var list = [].slice.call(photoDivs);
+
+        return list.map(function(div) {
+          return div.src;
+        });
+      })
+      .end()
+      .then(function(result) {
+        return result.slice(1,5);
+      })
+      .then(function (images) {
+        res.json(images);
+      })
+      .catch(function (error) {
+        console.error('Search Failed', error);
+      })
+  }
+  findHedgieImage(keyword);
+});
 
 // scraper endpoint
 
